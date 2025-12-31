282ae8974d853fa716e04fd432a3ae0e383803b9
hzqula <ilooqstrasi@gmail.com>
2025-12-31 17:15:59 +0700

    feat: update and responsive ui on main feature

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: any = {
         protocol: "https",
         hostname: "ytrxhrltkqztkcnqlcrz.storage.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "rskkqpmgscsyclbtnkci.storage.supabase.co",
+      },
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "tse2.mm.bing.net" },
       { protocol: "https", hostname: "tse1.mm.bing.net" },
